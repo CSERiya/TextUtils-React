9361540c86d393af5da2d630f47ec49416ab310b
Riya Kumari <riyakumari3043@gmail.com>
2023-08-08 20:56:36 +0530

    run

--- src/components/About.js
+++ src/components/About.js
@@ -1,52 +1,73 @@
-import React, { useEffect } from "react";
-import Accordion from "./Accordion";
-import PropTypes from "prop-types";
+import React from 'react'
 
-const About = ({ mode }) => {
-	const { darkOnLight } = mode;
+export default function About(props) {
 
-	useEffect(() => {
-		document.title = "TextUtils - FAQ";
-	}, []);
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // }) 
+    let myStyle = {
+        color: props.mode ==='dark'?'white':'#042743',
+        backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
+    }
+    
+    return (
+        <div className="container">
+            <h1 className="my-3" style={{color: props.mode ==='dark'?'white':'#042743'}}>About Us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse"
+                     data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <strong>Why use TextUtils? </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" 
+                    aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    Textutils gives you a way to analyze your text quickly and efficiently. Be it word count, 
+                    character count or time taken to read each word.
 
-	return (
-		<>
-			<h1 className={`text-${darkOnLight}`}>FAQs</h1>
-			<div className={`accordion my-3 text-${darkOnLight}`} id="faqAccordion">
-				<Accordion mode={mode} header="How this website works?" body={<>Initially, put some text in the text box 
-        then, choose the operation you wants to perform with your text. Then, text summary will provide some
-         details about your text.</>} count="One" accordionId="#faqAccordion" />
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" 
+                    data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" 
+                    aria-controls="collapseTwo">
+                    <strong>Is TextUtil Free to use? </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" 
+                    data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    TextUtils is a free character counter tool that provides instant character count & word 
+                    count statistics for a given text. TextUtils reports the number of words and characters. 
+                    Thus it is suitable for writing text with word/ character limit.
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed" style={myStyle} type="button"
+                     data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" 
+                     aria-controls="collapseThree">
+                    <strong>Is TextUtil Browser Compatible? </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" 
+                    aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    This word counter software works in any web browsers such as Chrome, Firefox, Internet 
+                    Explorer, Safari, Opera. It suits to count characters in facebook, blog, books, excel 
+                    document, pdf document, essays, etc.
 
-				<Accordion
-					mode={mode}
-					header="Is the website made on ReactJS?"
-					body={
-						<>
-							<strong>Yes,</strong> the website is completely made on ReactJS.
-						</>
-					}
-					count="Two"
-					accordionId="#faqAccordion"
-				/>
+                    </div>
+                    </div>
+                </div>
+            </div>
 
-				<Accordion
-					mode={mode}
-					header="Does this website use any framework for styling?"
-					body={
-						<>
-							<strong>Yes, </strong> the website using Bootstrap for all the styling and for some pre-default functionality provided by Bootstrap.
-						</>
-					}
-					count="Three"
-					accordionId="#faqAccordion"
-				/>
-			</div>
-		</>
-	);
-};
-
-About.propTypes = {
-	mode: PropTypes.object.isRequired,
-};
-
-export default About;
+        </div>
+    )
+}
