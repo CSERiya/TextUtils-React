0437bf0812d1938de3505fef0f5b71f3e4a8c68f
Riya <riyakumari3043@gmail.com>
2023-08-09 20:40:31 +0530

    run

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -56,10 +56,10 @@ export default function Navbar(props) {
   <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
   <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.mode==='light'?'Enable darkmode':'Enable lightmode'}</label>
 </div>
-      <form className="d-flex">
+      {/* <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-primary" type="submit">Search</button> 
-      </form>
+      </form> */}
     </div>
   </div>
 </nav>
