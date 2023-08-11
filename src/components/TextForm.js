8e2be04a5820fa7a8b91318b266f4c26e0821a2a
Riya <riyakumari3043@gmail.com>
2023-08-11 12:50:50 +0530

    run

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,15 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
+  const calculateReadingTime = (wordCount) => {
+    const wordsPerMinute = 200; // Average reading speed (adjust as needed)
+    const minutes = wordCount / wordsPerMinute;
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = Math.floor(minutes % 60);
+    const seconds = Math.floor((minutes * 60) % 60);
+
+    return { hours, minutes: remainingMinutes, seconds };
+  };
 const handleUpClick=()=>{
     let newText=text.toUpperCase();
     setText(newText);
@@ -24,7 +33,7 @@ navigator.clipboard.writeText(text);
 props.showAlert("Copied to clipboard","success");
 }
 
-const handleextraspace=()=>{
+const handleextraspace=()=>{ 
   let newText=text.split(/[ ]+/);
  setText(newText.join(" "))
  props.showAlert("extra spaces handled","success");
@@ -41,16 +50,26 @@ const cleartext=()=>{
   props.showAlert("Cleared text","success");
 }
 
-const handleOnChange=(event)=>{
-    setText(event.target.value);
+// const handleOnChange=(event)=>{
+//     setText(event.target.value);
    
   
-}
-    const [text, setText] = useState('');   
+// }
+const handleOnChange = (event) => {
+  const newText = event.target.value;
+  setText(newText);
+  const wordCount = newText.trim().split(/\s+/).filter((e) => e !== '').length;
+  setReadingTime(calculateReadingTime(wordCount));
+};
+
+const [text, setText] = useState('');
+const [readingTime, setReadingTime] = useState({ hours: 0, minutes: 0, seconds: 0 });
+
   return (
     <>
  
-<div className="container" style={{color:props.mode==='dark'?'white':'#042743'}}> 
+{/* <div className="container" style={{color:props.mode==='dark'?'white':'#042743'}}>  */}
+<div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
 <h1 className='mb-3'>{props.heading}</h1> 
 <div className="mb-3">
  
@@ -66,6 +85,7 @@ const handleOnChange=(event)=>{
 <button  disabled={text.length===0}className="btn btn-dark mx-2 my-1" onClick={cleartext}>Clear text</button>
 
     </div>
+    
     <div disabled={text.length===0}className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>
 <h2>Your text summary</h2>
 
@@ -73,11 +93,19 @@ const handleOnChange=(event)=>{
 {text.trim(/\s+/).split(/\s+/).filter((e)=>e!=="").length} <b>words</b> and  {text.length} <b>characters</b>
 </p>
 
-<p>{(0.008*(text.trim(/\s+/).split(/\s+/).filter((e)=>e!=="").length)).toFixed(3)} <b>Minutes read</b></p>
+{/* <span>{((0.008*(text.trim(/\s+/).split(/\s+/).filter((e)=>e!=="").length))/60).toFixed(0)} <b> hours read</b></span>
+<span> and </span>
+<span>{(0.008*(text.trim(/\s+/).split(/\s+/).filter((e)=>e!=="").length)).toFixed(0)} <b> minutes read</b></span>
+<span> and </span>
+<span>{((0.008*(text.trim(/\s+/).split(/\s+/).filter((e)=>e!=="").length))*60).toFixed(0)} <b> seconds read</b></span> */}
+ <span>
+          {readingTime.hours} <b>hours</b>, {readingTime.minutes} <b>minutes</b>, and {readingTime.seconds} <b>seconds read</b>
+        </span>
+
 <h2>Preview</h2>
 <p>{text.length>0?text:"Nothing to preview"}</p> 
     </div>
   </>
 
-  )
+  )  
 }
