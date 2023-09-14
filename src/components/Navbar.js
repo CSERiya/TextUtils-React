08ad7215db17ebf015d38a02af632e6be2132c51
CSERiya <93813209+CSERiya@users.noreply.github.com>
2023-09-14 12:21:46 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,10 +17,10 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/">Home</Link>
+          <Link className={`nav-link ${location.pathname==="/"?active:""}`} aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/about">{props.aboutText}</Link>
+          <Link className={`nav-link ${location.pathname==="/about"?active:""}`} to="/about">{props.aboutText}</Link>
         </li>
 
       </ul>
